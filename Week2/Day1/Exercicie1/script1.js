c64aa503191e00dbe94f16f07b854e231d9b6e61
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-08-24 19:29:35 +0000

    challenge day3 nija update

---
+++ Week2/Day1/Exercicie1/script1.js
@@ -0,0 +1,5 @@
+let adresseNuméro = "B0002";
+let rueAdresse = "Rue 01";
+let pays = "Burkina Faso";
+let globalAddress = adresseNuméro+" "+rueAdresse+" "+pays;
+alert("I live in"+" "+ " "+globalAddress)
---
+++ Week2/Day1/Exercicie1/script2.js
@@ -0,0 +1,4 @@
+let monAnneeNaissance = 2000;
+let anneeFuture = 2032;
+let monAgeFuture = anneeFuture-monAnneeNaissance;
+alert("mon annee de naissance est"+" "+monAnneeNaissance+" "+"et en"+" "+anneeFuture+" "+"mon age sera de"+" "+monAgeFuture+" "+"ans")
