a843be0188e6b2c49f452829359846239d75e6fd
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-08-29 13:44:43 +0000

    challenge week 2 day1

---
+++ Week2/Day1/Challenge/script.js
@@ -0,0 +1,29 @@
+//Exercice 1
+
+let fruits = ["Banana", "Apples", "Oranges", "Blueberries"];
+//Suprimer Banana
+fruits.splice(0,1);
+//Trier par ordre alphabetique
+fruits.sort()
+//Ajouter Kiwi
+fruits.push("Kiwi")
+
+//Suprimer Aplles avec une methode differente de la premiere
+
+
+//Trier par ordre inverse
+fruits.reverse()
+
+
+
+
+
+
+
+
+
+
+//Exercice 2
+
+let moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
+console.log(moreFruits[1][1])
