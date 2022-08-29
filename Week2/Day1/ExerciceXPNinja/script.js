1402083ab17d72a99b35b2da5ac8b711e6698c29
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-08-29 14:54:14 +0000

    exerxice XP Ninja week 2 day1

---
+++ Week2/Day1/ExerciceXPNinja/script.js
@@ -0,0 +1,55 @@
+    //Exercicie 1
+
+    5 >= 1
+
+    //prediction true car l'instruction compare si 5 est superieure a ou egale à 1
+    //réel true
+
+    0 === 1
+
+    //prediction false car l'instruction compare si 0 est egale à 1 en valeur et en type
+    //réel false
+
+    4 <= 1
+
+    //prediction false car l'instruction compare si 4 est inferieure a ou egale à 1
+    //réel false
+
+    1 != 1
+
+    //prediction false car l'instruction compare si 1 est different de 1
+    //réel false
+
+
+    "A" > "B"
+
+    //prediction false car l'instruction compare si A est superieure B selon la table ASCII
+    //réel false
+
+
+    "B" < "C"
+
+    //prediction true car l'instruction compare si B est inferieure à C selon la table ASCII
+    //réel true
+
+    "a" > "A"
+    //prediction true car l'instruction compare si a est superieure à A selon la table ASCII
+    //réel true
+
+
+    "b" < "A"
+
+    //prediction false car l'instruction compare si b est inferieure à A selon la table ASCII
+    //réel false
+
+
+    true === false
+
+    //prediction false car l'instruction compare si true valeur bool = 1 et false valeur bool =0 sont egaux en valeur et type
+    //réel false
+
+
+    true != true
+
+    //prediction false car l'instruction compare si true valeur bool = 1 est deferente de true valeur bool = 1 en valeur et en type
+    //réel false
