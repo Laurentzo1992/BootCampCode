84e50b9b0e8fe1ac938719bc2a12a71a379a4c55
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-09-12 12:56:24 +0000

    week4day3 ExerciceXp

---
+++ Week4/Day3/ExercicieXp/ExerciceXp.py
@@ -0,0 +1,29 @@
+#Exercice 1
+
+# keys = ['Ten', 'Twenty', 'Thirty']
+# values = [10, 20, 30]
+# ZipList = dict(zip(keys, values))
+# print(ZipList)
+
+#Exercice 2
+
+# valuepay = list()
+# ageMoin3 = 0
+# ageCompris12 = 10
+# agePlusDe12 = 15
+# family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+# for value in family.values():
+#     if value >= 0 and value <3:
+#         print("Le ticket est gratuit :")
+#         valuepay.append(ageMoin3)
+#     elif value >= 3 and value <= 12:
+#         print(f"Le ticket coute {ageCompris12}$")
+#         valuepay.append(ageCompris12)
+#     else:
+#         print(f"Le ticket coute {agePlusDe12}$")
+#         valuepay.append(agePlusDe12)
+# print(f"La liste des couts par membre {valuepay}")
+# print(f"Cout total pour la famile {sum(valuepay)}$")
+
+
+
