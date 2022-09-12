4c1b4168292ee2df7b350df084eba0fbe1fcb2b2
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-09-12 19:30:30 +0000

    week4day3 ExerciceXp

--- Week4/Day3/ExercicieXp/ExerciceXp.py
+++ Week4/Day3/ExercicieXp/ExerciceXp.py
@@ -1,34 +1,35 @@
 #Exercice 1
-
-# keys = ['Ten', 'Twenty', 'Thirty']
-# values = [10, 20, 30]
-# ZipList = dict(zip(keys, values))
-# print(ZipList)
+print("Exercicie 1\n")
+keys = ['Ten', 'Twenty', 'Thirty']
+values = [10, 20, 30]
+ZipList = dict(zip(keys, values))
+print(ZipList)
+print("\n")
 
 #Exercice 2
-
-# valuepay = list()
-# ageMoin3 = 0
-# ageCompris12 = 10
-# agePlusDe12 = 15
-# family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
-# for value in family.values():
-#     if value >= 0 and value <3:
-#         print("Le ticket est gratuit :")
-#         valuepay.append(ageMoin3)
-#     elif value >= 3 and value <= 12:
-#         print(f"Le ticket coute {ageCompris12}$")
-#         valuepay.append(ageCompris12)
-#     else:
-#         print(f"Le ticket coute {agePlusDe12}$")
-#         valuepay.append(agePlusDe12)
-# print(f"La liste des couts par membre {valuepay}")
-# print(f"Cout total pour la famile {sum(valuepay)}$")
-
+print("Exercicie 2\n")
+valuepay = list()
+ageMoin3 = 0
+ageCompris12 = 10
+agePlusDe12 = 15
+family = {"rick": 43, 'beth': 13, 'morty': 5, 'summer': 8}
+for value in family.values():
+    if value >= 0 and value <3:
+        print("Le ticket est gratuit :")
+        valuepay.append(ageMoin3)
+    elif value >= 3 and value <= 12:
+        print(f"Le ticket coute {ageCompris12}$")
+        valuepay.append(ageCompris12)
+    else:
+        print(f"Le ticket coute {agePlusDe12}$")
+        valuepay.append(agePlusDe12)
+print(f"La liste des couts par membre {valuepay}")
+print(f"Cout total pour la famile {sum(valuepay)}$")
+print("\n")
 
 
 #Exercicie 3
-
+print("Exercicie 3\n")
 #Créez un dictionnaire appelé branddont la valeur correspond 
 #aux informations de la première partie (transformez les informations 
 #en clés et valeurs ).
@@ -73,29 +74,29 @@ brand.update(more_on_zara)
 print(f" La nouvelle valeur du nombre de magasin est : brand['number_stores']")
 #print(brand)
 #print(more_on_zara)
-
+print("\n")
 
 
 
 #Exercice 4
-
-# users = ["Mickey","Minnie","Donald","Ariel","Pluto"]
-# #Utilisez une boucle for pour recréer le 1er résultat. Astuce : ne codez pas les numéros en dur.
-# disney_users_A = {users[i]: i for i in range(len(users))}
-# print(disney_users_A)
-# #Utilisez une boucle for pour recréer le 2ème résultat. Astuce : ne codez pas les numéros en dur.
-# disney_users_B = {i: users[i] for i in range(len(users))}
-# print(disney_users_B)
-# #Utilisez une méthode pour recréer le 3ème résultat. Astuce : Le 3ème résultat #est trié par ordre alphabétique.
-# disney_users = sorted(disney_users_A, reverse=False)
-# disney_users_C = {disney_users[i]: i for i in range(len(disney_users))}
-# print(disney_users_C)
-# #Les caractères dont les noms contiennent la lettre "i".
-# disney_users_D = {users[i]: i for i in range(len(users)) if "i" in users[i]}
-# print(disney_users_D)
-# #Les caractères, dont les noms commencent par la lettre « m » ou « p ».
-# disney_users_E = {users[i]: i for i in range(len(users)) if users[i][2] == "m" or users[i][0] == "p"}
-# print(disney_users_E)
-
+print("Exercicie 4\n")
+users = ["Mickey","Minnie","Donald","Ariel","Pluto"]
+#Utilisez une boucle for pour recréer le 1er résultat. Astuce : ne codez pas les numéros en dur.
+disney_users_A = {users[i]: i for i in range(len(users))}
+print(disney_users_A)
+#Utilisez une boucle for pour recréer le 2ème résultat. Astuce : ne codez pas les numéros en dur.
+disney_users_B = {i: users[i] for i in range(len(users))}
+print(disney_users_B)
+#Utilisez une méthode pour recréer le 3ème résultat. Astuce : Le 3ème résultat #est trié par ordre alphabétique.
+disney_users = sorted(disney_users_A, reverse=False)
+disney_users_C = {disney_users[i]: i for i in range(len(disney_users))}
+print(disney_users_C)
+#Les caractères dont les noms contiennent la lettre "i".
+disney_users_D = {users[i]: i for i in range(len(users)) if "i" in users[i]}
+print(disney_users_D)
+#Les caractères, dont les noms commencent par la lettre « m » ou « p ».
+disney_users_E = {users[i]: i for i in range(len(users)) if users[i][2] == "m" or users[i][0] == "p"}
+print(disney_users_E)
+print("\n")
 
 
