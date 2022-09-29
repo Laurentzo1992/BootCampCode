81c4d070ff0fee554c18eff860765d793462067b
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-09-29 15:32:25 +0000

    week8day1 Challenge

--- Week8/Day1/Challenge/people/urls.py
+++ Week8/Day1/Challenge/people/urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from . import views
 
+urlpatterns = [
+	path('', views.people, name="people"),
+  	path('people/<int:id>/', views.people_one, name="people_one"),
+]
--- Week8/Day1/Challenge/people/views.py
+++ Week8/Day1/Challenge/people/views.py
@@ -1,3 +1,68 @@
 from django.shortcuts import render
+from django.http import request, HttpResponse, response
+
+
+
+def people(request):
+	people = [
+	  {
+	    'id': 1,
+	    'name': 'Bob Smith',
+	    'age': 35,
+	    'country': 'USA'
+	  },
+	  {
+	    'id': 2,
+	    'name': 'Martha Smith',
+	    'age': 60,
+	    'country': 'USA'
+	  },
+	  {
+	    'id': 3,
+	    'name': 'Fabio Alberto',
+	    'age': 18,
+	    'country': 'Italy'
+	  },
+	  {
+	    'id': 4,
+	    'name': 'Dietrich Stein',
+	    'age': 85,
+	    'country': 'Germany'
+	  }
+	]
+	sortedby_age = sorted(people, key = lambda d: d['age'])
+	return render(request, "people/peopl.html", {"people":sortedby_age})
+
+
+def people_one(request, id=id):
+    people = [
+	{
+	    'id': 1,
+	    'name': 'Bob Smith',
+	    'age': 35,
+	    'country': 'USA'
+	},
+	{
+	    'id': 2,
+	    'name': 'Martha Smith',
+	    'age': 60,
+	    'country': 'USA'
+	},
+	{
+	    'id': 3,
+	    'name': 'Fabio Alberto',
+	    'age': 18,
+	    'country': 'Italy'
+	},
+	{
+	    'id': 4,
+	    'name': 'Dietrich Stein',
+	    'age': 85,
+	    'country': 'Germany'
+	}
+	]
+    
+    people_one_list = filter(people, key = lambda d: d['id'])
+    return render(request, "people/peopl.html", {"people":people_one_list})
 
 # Create your views here.
