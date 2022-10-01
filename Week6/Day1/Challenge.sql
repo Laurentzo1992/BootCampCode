4b0d8aff696afb8c3eecdf7017f8cb297440bcf0
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-10-01 00:58:50 +0000

    week6day1 Challenge terminé

---
+++ Week6/Day1/Challenge.sql
@@ -0,0 +1,7 @@
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('ZONGO','Elie','06/08/1991', 5);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES(NULL, NULL,'06/08/1991', 5);
+
+-- L'enregistremnt va echouer car les cahmps sont pas nullable
