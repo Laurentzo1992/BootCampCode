e31ba7d6e393c05d3db53b2d5da40afa8ef94af9
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-10-04 00:55:22 +0000

    week2day2 ExerciceXPOR

--- Week2/Day2/ExerciciesXPGold/script.js
+++ Week2/Day2/ExerciciesXPGold/script.js
@@ -15,3 +15,27 @@ else if(langueChoisie=="hebreu"){
 else{
     alert("01110011 01101111 01110010 01110010 01111001");
 }
+
+//Exercice 2
+
+let userNote = prompt("Quelle votre note: ?");
+userNote = parseInt(userNote);
+
+if(userNote>90){
+    console.log("A")
+}
+else if(userNote>80 && userNote<=90){
+    console.log("B")
+}
+else if(userNote>=70 && userNote>=80){
+    console.log("C")
+}
+else {
+    console.log("D")
+}
+
+//Exercice 3
+
+let chaine = prompt("Saisir une chaine: ?");
+
+if(chaine.length>=3 && chaine.length)
