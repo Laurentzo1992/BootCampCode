c9ccccd0ac4e9750395a44de48685acc3f3b1dba
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-10-04 12:01:01 +0000

    week3day1

--- Week3/Day1/ExerciceXP/Exercice1/index.js
+++ Week3/Day1/ExerciceXP/Exercice1/index.js
@@ -1,3 +1,13 @@
-//alert(document.getElementById('container').innerHTML)
-let elem = document.getElementsByTagName('list')[0].innerHTML
-alert(elem[0].innerHTML = 'aaa')
+//1
+alert(document.getElementById('container').innerHTML)
+//2
+document.getElementsByTagName('li')[0].innerHTML = "Zongo"
+//3
+
+//4
+let saraparent = document.getElementsByTagName('ul')[1];
+let saraNode = document.getElementsByTagName('li')[3];
+let remov = saraparent.removeChild(saraNode);
+
+
+
