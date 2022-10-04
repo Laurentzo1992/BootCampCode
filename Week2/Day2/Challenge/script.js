5cf79d80b3c0f7856867bce36ae065c8faa1be12
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-10-04 00:24:23 +0000

    week2day2 challenge

--- Week2/Day2/Challenge/script.js
+++ Week2/Day2/Challenge/script.js
@@ -1,1 +1,24 @@
-let sentence = "i am not a bad man";
+//1
+	let sentence =prompt( "Entrez une phrase contenant le mot not et bad", "The movie is not that bad, I like it");
+	sentence = sentence.split(" ");
+//2
+	let wordNot = sentence.indexOf("not");
+//3
+	let wordBad = sentence.indexOf("bad");
+	if (wordBad) {
+		wordBad = sentence.indexOf("bad,");
+	}
+//4
+	if (wordBad > wordNot && wordBad != -1 && wordNot != -1) {
+		sentence.splice(wordNot,wordBad-wordNot+1,"good");
+		sentence = sentence.join(" ");
+	} else if (wordBad) {
+		wordBad = sentence.indexOf("bad,");
+		sentence.splice(wordNot,wordBad-wordNot+1,"good,");
+		sentence = sentence.join(" ");	
+	}
+//5
+	else {
+		sentence = sentence.join(" ");
+	}
+	console.log(sentence);
