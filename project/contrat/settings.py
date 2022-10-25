9e024ac299689aee6c517c228baba979321a996c
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-10-25 17:09:21 +0000

    formulaire engrestrement du contrat

--- project/contrat/settings.py
+++ project/contrat/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
@@ -110,6 +111,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "contrat/static")
