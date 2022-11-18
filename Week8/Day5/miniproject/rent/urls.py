0c38399960fbab3bf3c5dc27bcdb3135c7489503
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-11-18 18:37:35 +0000

    initial mini project file

--- Week8/Day5/miniproject/rent/urls.py
+++ Week8/Day5/miniproject/rent/urls.py
@@ -4,6 +4,16 @@ from  . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('rent/rental', views.rental, name='rental'),
+    path('rent/rental/<int:id>', views.rental_detail, name='rental_detail'),
+    path('rent/rental/add', views.add_rent, name='add_rent'),
+    
+    path('rent/customer/add', views.add_cust, name='add_cust'),
+    path('rent/customer', views.customer, name='customer'),
+    path('rent/customer/<int:id>', views.customer_detail, name='customer_detail'),
+    
+    path('rent/vehicle/add', views.add_vehi, name='add_vehi'),
+    path('rent/vehicle', views.vehicle, name='vehicle'),
+    path('rent/vehicle/<int:id>', views.vehicle_detail, name='vehicle_detail'),
     
 ]
   
--- Week8/Day5/miniproject/rent/views.py
+++ Week8/Day5/miniproject/rent/views.py
@@ -1,12 +1,85 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import LocationForm, VehiculeForm, ClientForm
+from django.contrib import  messages
 
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'rent/home.html')
 
 
 def rental(request):
     context ={}
-    return render(request, 'rental.html', context)
+    return render(request, 'rent/rental.html', context)
+
+
+def rental_detail(request, id):
+    context ={}
+    return render(request, 'rent/rental_detail.html', context)
+
+
+
+
+def add_rent(request):
+    if request.method=="POST":
+        form = LocationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Location effectué")
+            return redirect('type')
+        else:
+            return render(request, 'rent/add.html', {"form":form})
+    else:
+        form = LocationForm()
+        return render(request, 'rent/add.html', {"form":form})
+    
+    
+def customer(request):
+    context ={}
+    return render(request, 'rent/customer.html', context)
+
+
+def customer_detail(request, id):
+    context ={}
+    return render(request, 'rent/customer_detail.html', context)
+
+  
+def add_cust(request):
+    if request.method=="POST":
+        form = ClientForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Location effectué")
+            return redirect('customer')
+        else:
+            return render(request, 'rent/add_cust.html', {"form":form})
+    else:
+        form = ClientForm()
+        return render(request, 'rent/add_cust.html', {"form":form})
+    
+
+
+def vehicle(request):
+    context ={}
+    return render(request, 'rent/vehicle.html', context)
+
+
+def vehicle_detail(request, id):
+    context ={}
+    return render(request, 'rent/vehicle_detail.html', context)
+
+
+def add_vehi(request):
+    if request.method=="POST":
+        form = VehiculeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "vehicule effectué")
+            return redirect('vehicle')
+        else:
+            return render(request, 'rent/add_vehi.html', {"form":form})
+    else:
+        form = VehiculeForm()
+        return render(request, 'rent/add_vehi.html', {"form":form})
+    
 
 # Create your views here.
