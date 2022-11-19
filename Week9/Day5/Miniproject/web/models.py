e292f20599985d10fd1351d1c5b908a81c67b74b
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-11-19 14:34:29 +0000

    initial hotl torquay project

--- Week9/Day5/Miniproject/web/models.py
+++ Week9/Day5/Miniproject/web/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+
+class Hotel(models.Model):
+    nom = models.CharField(max_length=200)
+    emplacement = models.CharField(max_length=200)
+    description = models.TextField()
+    disponible = models.BooleanField(default=True)
+    image = models.ImageField(upload_to='hotel_img', null=True, blank=True)
+    
+    def __str__(self):
+        return self.nom
+    
+
+class Reservation(models.Model):
+    hotel = models.ForeignKey(Hotel, null=True, blank=True, on_delete=models.CASCADE)
+    avis =models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.hotel
+
 # Create your models here.
