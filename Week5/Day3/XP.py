e83b42acf8c20ac019b4fc30485823279d886e34
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-12-04 12:00:00 +0000

    week5 day3

--- Week5/Day3/XP.py
+++ Week5/Day3/XP.py
@@ -14,3 +14,41 @@ Doc = Documentation()
 print( Doc.__doc__())
 
 #Exercice 2
+
+class Currency:
+	
+    def __init__(self, currency, amount):
+        self.currency = currency
+        self.amount = amount
+
+    def __str__(self):
+        if self.amount > 1 :
+            return f"{self.amount} {self.currency}s"
+        else :
+            return f"{self.amount} {self.currency}"
+
+    def __repr__(self):
+        if self.amount > 1 :
+            return f"{self.amount} {self.currency}s"
+        else :
+            return f"{self.amount} {self.currency}"
+
+    def __int__(self):
+        return self.amount
+
+    def __add__(self, other):
+        a=True
+        try :
+            if self.currency == other.currency :
+                print(self.amount + other.amount)
+                return Currency(self.currency,self.amount + other.amount)
+            else :
+                a=False
+        except :
+            if a :
+                print(self.amount + other)
+                return Currency(self.currency,self.amount + other)
+        if not a :
+            raise TypeError("Cannot add between Currency type <dollar> and <shekel>")
+
+
