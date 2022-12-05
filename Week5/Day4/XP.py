039828311b54e6868ffd33b66a5da227e859bb95
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-12-05 20:55:29 +0000

    week5 day 4

--- Week5/Day4/XP.py
+++ Week5/Day4/XP.py
@@ -7,9 +7,26 @@ def get_words_from_file():
 get_words_from_file()
 
 
+import random
+from random import randint
+def get_random_sentence(length):
+    phrase = []
+    for i in range(length):
+        phrase = phrase.append(get_words_from_file().random.randint(0,len(mots)))
+        return phrase.join(" ")
+get_random_sentence(6)
 
 
+def main():
+    print("\n Dans mon programme vous génèrez une phrase\n"
+          "en specifiant sa taille le programme vous le contitue.\n")
+    length = int(input('Saisir la taille (2 à 20) : '))
+    if length >= 2 and length <= 20:
+        print(get_random_sentence(length))
+    else:
+        print('incorrectes')
 
+main()
 
 #Exercice 2
 
