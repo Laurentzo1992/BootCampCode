2c289142c1368665c7fe1a554db018ad9ba3d9d5
Laurentzo1992 <lnikiema9@gmail.com.com>
2022-12-06 01:21:28 +0000

    week 5 day4

--- Week5/Day4/Defis.py
+++ Week5/Day4/Defis.py
@@ -1,3 +1,14 @@
 class Text:
     def __init__(self):
         self.chaine = "une méthode pour renvoyer la fréquence d'un mot dans le texte"
+    
+    def frequence():
+        pass
+    
+    
+    def motcourant():
+        pass
+    
+    
+    def mot unique():
+        pass
